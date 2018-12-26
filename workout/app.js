6b4936874d3e951498a293391c3f08836f57bc6d
Zach Smith <zsmith126028@gmail.com>
2018-12-25 18:18:20 -0600

    add event listeners in function. add addRow function. add addResult function

--- workout/app.js
+++ workout/app.js
@@ -1,4 +1,5 @@
 // Define UI variables
+const newRowForm = document.querySelector("#new-row-form");
 const resultsForm = document.querySelector("#result-form");
 const resultsTable = document.querySelector(".results-table");
 const inputs = document.querySelectorAll('input[type="text"]');
@@ -7,9 +8,37 @@ const inputs = document.querySelectorAll('input[type="text"]');
 //   storeResultInLocalStorage(inputs[i].value);
 // }
 
+loadEventListeners();
+
+// load all event listeners 
+function loadEventListeners() {
+  // add new row event
+  newRowForm.addEventListener("submit", addRow);
+  // add results to local storage
+  resultsForm.addEventListener("submit", addResult);
+}
+
+// add new row to spreadsheet
+function addRow(e) {
+  // create tr element
+  const tr = document.createElement("tr");
+  // add tr html
+  tr.innerHTML =
+    "<td><input type='text'></td><td><input type='text'></td><td><input type='text'></td>";
+  // append tr to tbody parent
+  resultsTable.appendChild(tr);
+
+  e.preventDefault();
+}
+
 // Add results
 function addResult(e) {
-  
+  // store in local storage
+  for (let i = 0; i < inputs.length; i++) {
+    storeResultInLocalStorage(inputs[i].value);
+  }
+
+  e.preventDefault();
 }
 
 // store results from workout in local storage
