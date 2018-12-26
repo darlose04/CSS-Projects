fee3eacfe7c82bb80db54b37aa14f606ad23d354
Zach Smith <zsmith126028@gmail.com>
2018-12-25 22:14:50 -0600

    create nested loops to iterate through local storage values and place into tr tds

--- workout/app.js
+++ workout/app.js
@@ -18,16 +18,44 @@ function loadEventListeners() {
 
 // get results from local storage
 function getResults() {
-  // want to loop through the local storage and apply the values to each td 
+  // want to loop through the local storage and apply the values to each td
 
   let results;
   if (localStorage.getItem("results") === null) {
     results = [];
   } else {
     results = JSON.parse(localStorage.getItem("results"));
-    
   }
-  console.log(results);
+  // console.log(results);
+
+  // loop through the results array taken from local storage
+  for (let h = 0; h <= results.length; h++) {
+    // counter variable for while loop
+    let i = 0;
+    let rows = [];
+
+    // create tr element to hold tds for row values
+    const tr = document.createElement("tr");
+    resultsTable.appendChild(tr);
+
+    // use while loop to push every three results values into empty rows array
+    while (i < 3) {
+      rows.push(results.shift());
+      i++;
+    }
+
+    // create a td inside the above tr for every value in rows
+    for (let k = 0; k < rows.length; k++) {
+      let td = document.createElement("td");
+      td.innerText = rows[k];
+      tr.appendChild(td);
+    }
+
+    console.log(rows);
+    // set counter variable back to 0 and empty rows array
+    i = 0;
+    rows = [];
+  }
 }
 
 // add new row to spreadsheet
@@ -39,7 +67,6 @@ function addRow(e) {
     "<td><input type='text'></td><td><input type='text'></td><td><input type='text'></td>";
   // append tr to tbody parent
   resultsTable.appendChild(tr);
-  // storeResultInLocalStorage(tr);
 
   e.preventDefault();
 }
