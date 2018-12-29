754122b70d91ca4442a6f53e5602fc05c6e2843e
Zach Smith <zsmith126028@gmail.com>
2018-12-28 18:54:53 -0600

    add event listeners

--- workout/lifts.js
+++ workout/lifts.js
@@ -4,3 +4,16 @@ const newLiftButton = document.querySelector("#new-lift");
 const saveLiftsButton = document.querySelector("#save-lifts");
 const exercisesTable = document.querySelector(".exercises-table");
 
+loadEventListeners();
+
+// load all event listeners
+function loadEventListeners() {
+  // DOM load event
+  document.addEventListener("DOMContentLoaded", getLifts);
+  // add/save muscle group ...
+  
+  // add new lift event
+  newLiftButton.addEventListener("click", addLift);
+  // add lifts to local storage event
+  saveLiftsButton.addEventListener("click", saveLift);
+}
