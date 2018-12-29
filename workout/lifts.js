3b1892c1212d8d78499dce9a2d6785d3f859ad27
Zach Smith <zsmith126028@gmail.com>
2018-12-28 18:49:12 -0600

    add ui vars

---
+++ workout/lifts.js
@@ -0,0 +1,6 @@
+// Define UI variables
+const muscleGroup = document.querySelector(".muscle-group");
+const newLiftButton = document.querySelector("#new-lift");
+const saveLiftsButton = document.querySelector("#save-lifts");
+const exercisesTable = document.querySelector(".exercises-table");
+
