fd36dc569287c87dd120bb9c2caeee08178c0554
Zach Smith <zsmith126028@gmail.com>
2019-01-04 22:51:50 -0600

    remove buttons from innerHTML when displaying days from local storage

--- workout/js/app.js
+++ workout/js/app.js
@@ -58,7 +58,7 @@ function addDay(e) {
   div.className = "day";
   // add div innerHTML
   div.innerHTML =
-    '<table class="exercises"><thead><tr><th class="muscle-group"><input class="muscle-value" type="text"></th><th class="new-lift"><button class="button">New Lift</button></th><th class="save-lifts"><button class="button">Save</button></th></tr></thead><thead><tr><th>Exercises</th><th>Sets</th><th>Reps</th></tr></thead><tbody class="exercises-table"></tbody></table><table class="results"><thead><tr><th class="date"><input type="date"></th><th id="new-row"><button class="button">New Row</button></th><th id="results"><button class="button">Save</button></th></tr></thead><thead><tr><th>Weight</th><th>Sets</th><th>Reps</th></tr></thead><tbody class="results-table"></tbody></table>';
+    '<table class="exercises"><thead><tr><th class="muscle-group"><input class="muscle-value" type="text"></th></tr></thead><thead><tr><th>Exercises</th><th>Sets</th><th>Reps</th></tr></thead><tbody class="exercises-table"></tbody></table><table class="results"><thead><tr><th class="date"><input type="date"></th><th id="new-row"><button class="button">New Row</button></th><th id="results"><button class="button">Save</button></th></tr></thead><thead><tr><th>Weight</th><th>Sets</th><th>Reps</th></tr></thead><tbody class="results-table"></tbody></table>';
   
   saveDaysInLocalStorage(div.innerHTML);
 }
