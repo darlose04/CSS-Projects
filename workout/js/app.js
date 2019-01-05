a7c67835cc73fa31bdcc626d0e7ed3a1fc374aa2
Zach Smith <zsmith126028@gmail.com>
2019-01-04 18:51:07 -0600

    change ui var to weekDiv. add function to add new day

--- workout/js/app.js
+++ workout/js/app.js
@@ -1,6 +1,6 @@
 // Define UI variables
 let newDayButton = document.querySelector(".new-day");
-let dayDiv = document.querySelector(".day");
+let weekDiv = document.querySelector(".week");
 
 loadEventListeners();
 
@@ -10,5 +10,13 @@ function loadEventListeners() {
 }
 
 function addNewDay(e) {
-  
+  // create div element
+  let div = document.createElement("div");
+  // add class
+  div.className = "day";
+  // add div innerHTML
+  div.innerHTML = '<table class="exercises"><thead><tr><th class="muscle-group"><input class="muscle-value" type="text"></th><th class="new-lift"><button class="button">New Lift</button></th><th class="save-lifts"><button class="button">Save</button></th></tr></thead><thead><tr><th>Exercises</th><th>Sets</th><th>Reps</th></tr></thead><tbody class="exercises-table"></tbody></table><table class="results"><thead><tr><th class="date"><input type="date"></th><th id="new-row"><button class="button">New Row</button></th><th id="results"><button class="button">Save</button></th></tr></thead><thead><tr><th>Weight</th><th>Sets</th><th>Reps</th></tr></thead><tbody class="results-table"></tbody></table>'
+
+  // append div to div.week
+  weekDiv.appendChild(div);
 }
