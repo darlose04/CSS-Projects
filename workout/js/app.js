c8bad7051dd881bcbe5cf7eb367466d961745105
Zach Smith <zsmith126028@gmail.com>
2019-01-09 15:26:22 -0600

    remove results buttons from table when reloading saved day

--- workout/js/app.js
+++ workout/js/app.js
@@ -58,7 +58,7 @@ function addDay(e) {
   div.className = "day";
   // add div innerHTML
   div.innerHTML =
-    '<table class="exercises"><thead><tr><th class="muscle-group"><input class="muscle-value" type="text"></th></tr></thead><thead><tr><th>Exercises</th><th>Sets</th><th>Reps</th></tr></thead><tbody class="exercises-table"></tbody></table><table class="results"><thead><tr><th class="date"><input type="date"></th><th id="new-row"><button class="button">New Row</button></th><th id="results"><button class="button">Save</button></th></tr></thead><thead><tr><th>Weight</th><th>Sets</th><th>Reps</th></tr></thead><tbody class="results-table"></tbody></table>';
+    '<table class="exercises"><thead><tr><th class="muscle-group"><input class="muscle-value" type="text"></th></tr></thead><thead><tr><th>Exercises</th><th>Sets</th><th>Reps</th></tr></thead><tbody class="exercises-table"></tbody></table><table class="results"><thead><tr><th class="date"><input type="date"></th></tr></thead><thead><tr><th>Weight</th><th>Sets</th><th>Reps</th></tr></thead><tbody class="results-table"></tbody></table>';
   
   saveDaysInLocalStorage(div.innerHTML);
 }
@@ -78,7 +78,6 @@ function saveDaysInLocalStorage(day) {
 }
 
 // This function contains the code for the exercises table
-
 function liftsJS() {
   // Define UI variables
   let newLiftButton = document.querySelector(".new-lift");
