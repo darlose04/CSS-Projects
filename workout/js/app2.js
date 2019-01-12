536918633f25a5b99bf2e789175660f32c068304
Zach Smith <zsmith126028@gmail.com>
2019-01-12 10:29:54 -0600

    add UI variables for day buttons, lift table, results table

---
+++ workout/js/app2.js
@@ -0,0 +1,14 @@
+// UI Variables for New Day
+let newDayButton = document.querySelector(".new-day");
+let saveDayButton = document.querySelector(".save-day");
+let weekDiv = document.querySelector(".week");
+
+// UI Variables for Lifts Table
+let newLiftButton = document.querySelector(".new-lift");
+let saveLiftsButton = document.querySelector(".save-lifts");
+let exercisesTable = document.querySelector(".exercises-table");
+
+// UI Variables for Results Table
+let newRowButton = document.querySelector(".new-row");
+let resultsButton = document.querySelector(".results");
+let resultsTable = document.querySelector(".results-table");
